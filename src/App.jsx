dd904cc54eba382c6ad42a82e33b145206399cba
Chris Grace <chris.grace@tghp.co.uk>
2023-10-27 21:47:50 +0100

    Add NewsHeadlines component

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { useState } from 'react'
+import NewsHeadlines from './components/NewsHeadlines'
 
 import newsLogo from './assets/images/logo.svg'
 import './assets/sass/main.scss'
@@ -9,9 +10,10 @@ function App() {
   return (
     <div className="news-app">
       <div className="news-app__header">
-        <img src={newsLogo} className="logo react" alt="Global News" />
+        <img src={newsLogo} alt="Global News" />
         <h1>Global Top News</h1>
       </div>
+      <NewsHeadlines />
     </div>
   )
 }
---
+++ src/components/NewsHeadlines.jsx
@@ -0,0 +1,8 @@
+export default function NewsHeadlines() {
+  return (
+    <section className="news-app__news">
+      <h2>Top News in [Country]</h2>
+      <p>No News Available</p>
+    </section>
+  );
+}
