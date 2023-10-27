23f13a13116234c54c6850e528d78f1678b7a1ef
Chris Grace <chris.grace@tghp.co.uk>
2023-10-27 22:12:46 +0100

    Slice the top 5 headlines and map them out in NewsHeadlines

--- src/components/NewsHeadlines.jsx
+++ src/components/NewsHeadlines.jsx
@@ -1,9 +1,19 @@
 export default function NewsHeadlines({ headlines }) {
-  console.log(headlines)
+  const topHeadlines = headlines.slice(0, 5);
+
   return (
     <section className="news-app__news">
       <h2>Top News in [Country]</h2>
-      <p>No News Available</p>
+      {!headlines && <p>No News Available</p>}
+      {headlines &&
+        <div className="news-app__news-items">
+          {topHeadlines.map(({ title, publishedAt, url }) => (
+            <div className="news-app__news-items news-headline" key={`${publishedAt}-${url}`}>
+              <h3>{title}</h3>
+            </div>
+          ))}
+        </div>
+      }
     </section>
   );
 }
